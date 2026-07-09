21864153025a7d92cebad2e131708eade4b64272
GODGIRII <rishabgirii28@gmail.com>
2026-07-09 22:48:27 +0530

    initial commit

---
+++ grade_checker.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// To make the temperature calculator
+// consider these variables
+// 1) Degree Celcius/ferenheitt
+// 2) initial and Final temp
+// 3) calculate the change in temp
+
+func temperatureAdjuster() (func (change float64) float64, float64) {
+	initialTemp := 36.5
+
+	adjustTemp := func(change float )
+}
+func main() {
+
+}
+
